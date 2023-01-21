c42c8b038cfaaaa3491577b19ddcee2931f13147
Oscar Martinez <oscarmartincol@outlook.com>
2023-01-20 20:55:11 -0500

    Primer commit: creacion de la sección Home, se configura el cambio de color con locaEstorage para almacenar la opción seleccionada.

---
+++ js/colores.js
@@ -0,0 +1,20 @@
+var switchColor = document.querySelector('.switch-color input[type="checkbox"]');
+var body = document.getElementById("body");
+function cambiarColor(e){
+    if(e.target.checked){
+        body.classList.add("dark");
+        localStorage.setItem("night-mode", "true");
+    }else{
+        if(body.classList.contains("dark")){
+            body.classList.remove("dark");
+            localStorage.setItem("night-mode", "false");
+        }
+    }
+
+}
+switchColor.addEventListener("change", cambiarColor, false);
+
+if(localStorage.getItem("night-mode") === "true"){
+    body.classList.add("dark");
+    switchColor.checked =true;
+}
